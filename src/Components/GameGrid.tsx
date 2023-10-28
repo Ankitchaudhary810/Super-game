3cba6dca5b920da2dfaf41baeadf1577cf7e821a
Ankit Chaudhary <ankitcha810@gmail.com>
2023-10-28 16:34:04 +0530

    get optimized image

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -6,7 +6,6 @@ const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <div>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
