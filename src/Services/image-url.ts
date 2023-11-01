740804759eb370cbcf134fcbc8c50e14bdf9bfe4
Ankit Chaudhary <ankitcha810@gmail.com>
2023-11-01 21:25:27 +0530

    handle games without image

--- src/Services/image-url.ts
+++ src/Services/image-url.ts
@@ -1,6 +1,7 @@
 
+import noImgae  from "../assets/logo.webp"
 const getCroppedImageUrl = (url: string) => {
-    if(!url) return "";
+    if(!url) return noImgae;
     const target = 'media/'
     const index = url.indexOf(target) + target.length;
     return url.slice(0, index) +  'crop/600/400/' + url.slice(index)
