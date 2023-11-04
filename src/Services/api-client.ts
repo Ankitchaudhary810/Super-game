decca10942227c53add624b28847e1f744db1325
Ankit <124044913+Ankitchaudhary810@users.noreply.github.com>
2023-11-04 09:55:53 +0530

    Update api-client.ts
    updated the api key for locals

--- src/Services/api-client.ts
+++ src/Services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:"https://api.rawg.io/api",
     params:{
-        key:"c89299b7dcd2470f93f8a54c3aa84b5c",
+        key:"YOUR-API-KEY",
     }
 })
